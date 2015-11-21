154e607809f2e50bf39167c68f4d25f05a1c1a33
schmots1 <schmots@gmail.com>
2015-11-21 17:09:57 -0500

    Precheck dashboard work

---
+++ html/precheck.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "<table border=1>"; 
+
+//Storage Controller section
+$sql = "select severity,pre_check,description, count(*) from Transition_PreCheck_Details group by pre_check";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+while($row=$result->fetch_assoc()) {
+$severity = $row['severity'];
+echo "$severity";
+if ($severity == "Red"){
+echo "<tr bgcolor='red'><td>"  . $row['count(*)'] . "</td><td>" . $row['pre_check'] . "</td><td width=550px>" . $row['description'];
+}
+if ($severity == "Yellow"){
+echo "<tr bgcolor='yellow'><td>"  . $row['count(*)'] . "</td><td>" . $row['pre_check'] . "</td><td width=550px>" . $row['description'];
+}
+}
+echo "</td></tr>";
+//echo "$result</td>";
+}
+else {
+echo "0 results";
+}
+echo "</tr>";
+
+
+echo "</table>";
+$conn->close();
+?>
