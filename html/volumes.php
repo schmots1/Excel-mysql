46852edd6d6a4f5207583c022e7359b181abc82b
schmots1 <schmots@gmail.com>
2015-12-09 15:13:37 -0500

    share reports

--- html/volumes.php
+++ html/volumes.php
@@ -14,21 +14,21 @@ if ($conn->connect_error) {
 echo "<a href=dashboard.php?database=$dbname> <- Back to Dashboard</a>";
 echo "<table border=0>"; 
 //Storage Controller section
-echo "<tr bgcolor=grey><td>Filer</td><td>Volume</td><td>Block Type</td><td>Used Space</td><td>File Count</td><td>Aggregate</td><td>Number of Qtrees</td><td>Number of Luns</td><td>Number of Snapmirrors</td><td>Number of Snapvaults</td><td>State</td><td>Language</td><td>Vfiler</td>";
+echo "<tr bgcolor=grey><td>Filer</td><td>Volume</td><td>Block Type</td><td>Used Space</td><td>File Count</td><td>Aggregate</td><td>Security Style</td><td>Number of Qtrees</td><td>Number of Luns</td><td>Number of Snapmirrors</td><td>Number of Snapvaults</td><td>State</td><td>Language</td><td>Vfiler</td>";
 if (!empty($_GET['block'])) {
-$sql = "select storage_controller,aggregate_name,vfiler_name,name,workload,state,block_type,language,files_used,total_used_size_GB,total_luns,total_qtrees,total_snapmirrors,total_snapvaults from Volumes where `block_type` like $block order by storage_controller, block_type, name";
+$sql = "select storage_controller,aggregate_name,vfiler_name,name,workload,state,block_type,language,files_used,total_used_size_GB,total_luns,total_qtrees,security_style,total_snapmirrors,total_snapvaults from Volumes where `block_type` like $block order by storage_controller, block_type, name";
 }
 elseif (!empty($_GET['type'])) {
-$sql = "select storage_controller,aggregate_name,vfiler_name,name,workload,state,block_type,language,files_used,total_used_size_GB,total_luns,total_qtrees,total_snapmirrors,total_snapvaults from Volumes where `type` like 'trad' order by storage_controller, block_type, name";
+$sql = "select storage_controller,aggregate_name,vfiler_name,name,workload,state,block_type,language,files_used,total_used_size_GB,total_luns,total_qtrees,total_snapmirrors,security_style,total_snapvaults from Volumes where `type` like 'trad' order by storage_controller, block_type, name";
 }
 else{
-$sql = "select storage_controller,aggregate_name,vfiler_name,name,workload,state,block_type,language,files_used,total_used_size_GB,total_luns,total_qtrees,total_snapmirrors,total_snapvaults from Volumes order by storage_controller, block_type, name";
+$sql = "select storage_controller,aggregate_name,vfiler_name,name,workload,state,block_type,language,files_used,total_used_size_GB,total_luns,total_qtrees,total_snapmirrors,security_style,total_snapvaults from Volumes order by storage_controller, block_type, name";
 }
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 while($row=$result->fetch_assoc()) {
 $alt = ($coloralternator++ %2 ? "CCCCCE" : "FFFFFF");
-echo "<tr style=\"background:$alt\"><td>"  . $row['storage_controller'] . "</td><td>" . $row['name'] . "</td><td>" . $row['block_type'] . "</td><td>" . $row['total_used_size_GB'] . "</td><td>" . $row['files_used'] . "</td><td>" . $row['aggregate_name'] . "</td><td>" . $row['total_qtrees'] . "</td><td>" . $row['total_luns'] . "</td><td>" . $row['total_snapmirrors'] . "</td><td>" . $row['total_snapvaults'] . "</td><td>" . $row['state'] . "</td><td>" . $row['language'] . "</td><td>" . $row['vfiler_name'];
+echo "<tr style=\"background:$alt\"><td>"  . $row['storage_controller'] . "</td><td>" . $row['name'] . "</td><td>" . $row['block_type'] . "</td><td>" . $row['total_used_size_GB'] . "</td><td>" . $row['files_used'] . "</td><td>" . $row['aggregate_name'] . "</td><td>" . $row['security_style'] . "</td><td>" . $row['total_qtrees'] . "</td><td>" . $row['total_luns'] . "</td><td>" . $row['total_snapmirrors'] . "</td><td>" . $row['total_snapvaults'] . "</td><td>" . $row['state'] . "</td><td>" . $row['language'] . "</td><td>" . $row['vfiler_name'];
 }
 echo "</td></tr>";
 //echo "$result</td>";
