7b36af389dc24f524aec6dc97194200299083770
schmots1 <schmots@gmail.com>
2016-02-04 10:44:18 -0500

    Delta report v1

--- html/dashboard.php
+++ html/dashboard.php
@@ -3,6 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = $_GET['database'];
+$project = split ("\-", $dbname);
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,9 +13,8 @@ if ($conn->connect_error) {
 }
 $heading = split ("\-", $dbname);
 echo "<h1>$heading[1]</h1>";
-echo "<table><tr><td rowspan=2>";
+echo "<table><tr><td rowspan=3>";
 echo "<table border=1>"; 
-
 //Storage Controller section
 echo "<tr>";
 $sql = "select * from Storage_Controllers";
@@ -22,7 +22,6 @@ $result = $conn->query($sql);
 $num_rows = mysqli_num_rows($result);
 echo "<td style = 'width: 200px'><a href=controllers.php?database=$dbname>Storage Controllers</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //vFiler section
 echo "<tr>";
 $sql = "select * from Vfilers where vfiler_name not in (select storage_controller from Vfilers)";
@@ -30,7 +29,6 @@ $result = $conn->query($sql);
 $num_rows = mysqli_num_rows($result);
 echo "<td style = 'width: 200px'><a href=vfilers.php?database=$dbname>vFilers</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //Aggregate section
 echo "<tr>";
 $sql = "select * from Aggregates";
@@ -44,7 +42,6 @@ $result = $conn->query($sql);
 $num_rows=mysqli_num_rows($result);
 echo "<td>&nbsp&nbsp<a href=aggrs.php?database=$dbname&block='32-bit'>32-bit Aggregates</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //Volume section
 echo "<tr>";
 $sql = "SELECT name, block_type, total_used_size_GB FROM Volumes";
@@ -64,7 +61,6 @@ $result = $conn->query($sql);
 $num_rows = mysqli_num_rows($result);
 echo "<td>&nbsp&nbsp<a href=volumes.php?database=$dbname&type=trad>Traditional Volumes</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //Qtree section
 echo "<tr>";
 $sql = "select * from Qtrees";
@@ -72,7 +68,6 @@ $result = $conn->query($sql);
 $num_rows = mysqli_num_rows($result);
 echo "<td><a href=qtrees.php?database=$dbname>Qtrees</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //Lun section
 echo "<tr>";
 $sql = "select * from Luns";
@@ -80,7 +75,6 @@ $result = $conn->query($sql);
 $num_rows = mysqli_num_rows($result);
 echo "<td><a href=luns.php?database=$dbname>Luns</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //CIFS shares section
 echo "<tr>";
 $sql = "select * from CIFS_Shares where LineID is not null";
@@ -88,7 +82,6 @@ $result = $conn->query($sql);
 $num_rows = mysqli_num_rows($result);
 echo "<td><a href=shares.php?database=$dbname>Shares</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //Export section
 echo "<tr>";
 $sql = "select * from NFS_Exports";
@@ -96,7 +89,6 @@ $result = $conn->query($sql);
 $num_rows = mysqli_num_rows($result);
 echo "<td><a href=exports.php?database=$dbname>Exports</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //Totals section
 echo "<tr>";
 $sql = "select cast(sum(replace(total_used_size_GB,',','')) as decimal (10,2)) as total from Volumes";
@@ -113,7 +105,6 @@ else {
 echo "0 results";
 }
 echo "</tr>";
-
 //SnapMirror section
 echo "<tr>";
 $sql = "select * from SnapMirror";
@@ -121,7 +112,6 @@ $result = $conn->query($sql);
 $num_rows = mysqli_num_rows($result);
 echo "<td><a href=snapmirrors.php?database=$dbname>SnapMirrors</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //SnapVault section
 echo "<tr>";
 $sql = "select * from SnapVault";
@@ -129,7 +119,6 @@ $result = $conn->query($sql);
 $num_rows = mysqli_num_rows($result);
 echo "<td><a href=snapvaults.php?database=$dbname>SnapVaults</a></td><td>$num_rows</td>";
 echo "</tr>";
-
 //Transition PreCheck section
 echo "<tr>";
 $sql = "select * from Transition_PreCheck_Summary where severity like 'Red'";
@@ -143,7 +132,6 @@ echo "</tr><tr>";
 echo "<td></td><td bgcolor='yellow'>$num_rows</td>";
 echo "</tr></table></td><td valign='top'>";
 // Migration Suggestion section
-
 echo "<table border=1>";
 echo "<tr><td colspan=2 bgcolor=lightgrey>Migration Suggestions</td></tr>";
 $sql = "select `migration_methodology`, count(*) from Migration_Master_Volume_View group by `migration_methodology`";
@@ -168,8 +156,34 @@ else{
 echo "<tr><td colspan=2><a href=plancreate.php?database=$dbname>Create Migration Plan</a></td></tr>";
 }
 echo "</table>";
+
+// Delta section
+echo "<tr><td valign='top'><table border=1>";
+echo "<tr><td colspan=2 bgcolor=lightgrey>Changed Volumes</td></tr>";
+$sql = "SHOW DATABASES LIKE 'delta-" . $project[1] . "'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+$sql = "select count(*) from `up-" . $project[1] . "`.`Volumes` where name not in (select name from `delta-" . $project[1] . "`.`Volumes`)";
+$result = $conn->query($sql);
+while($row=$result->fetch_assoc()) {
+echo "<tr><td>Removed Volumes</td><td>" . $row['count(*)'] . "</td></tr>";
+}
+$sql = "select count(*) from `delta-" . $project[1] . "`.`Volumes` where name not in (select name from `up-" . $project[1] . "`.`Volumes`)";
+$result = $conn->query($sql);
+while($row=$result->fetch_assoc()) {
+echo "<tr><td>Added Volumes</td><td>" . $row['count(*)'] . "</td></tr>";
+}
+}
+else{
+echo "<tr><td colspan=2>Create and convert a delta</td></tr>";
+}
+echo "</table>";
+
+echo "</table>";
 echo "</td></tr>";
 echo "</table>";
 echo "<a href=index.php>Select another Dataset</a> <a href=verify.php?delete=$dbname>Delete this dataset</a>";
+
+
 $conn->close();
 ?>
